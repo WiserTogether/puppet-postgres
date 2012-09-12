45be734f3d45402220bb8e69b6719db899591c55
Erik LaBianca <erik.labianca@wisertogether.com>
2012-09-12 00:51:40 -0400

    stop trying to load postgres from nonexistant locations

--- manifests/pg_hba.pp
+++ manifests/pg_hba.pp
@@ -1,10 +1,6 @@
 define postgres::pg_hba() {
     File <| title == 'pg_hba' |> {
         source => [
-            "puppet:///files/postgres/host/pg_hba.conf.$title.$fqdn",
-            "puppet:///files/postgres/host/pg_hba.conf.$title.$hostname",
-            "puppet:///files/postgres/env/pg_hba.conf.$title.$environment",
-            "puppet:///files/postgres/pg_hba.conf.$title",
             "puppet:///modules/postgres/pg_hba.conf.$title",
         ],
     }
